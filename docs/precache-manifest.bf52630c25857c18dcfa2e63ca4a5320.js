84955542f51890938258ea419c33cfc3be5bdcfc
Aakash Sangwan <47134609+Aakash2408@users.noreply.github.com>
2020-05-21 00:23:10 +0530

    Add files via upload

---
+++ docs/precache-manifest.bf52630c25857c18dcfa2e63ca4a5320.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d71ab7c182e03b92ec190507ad7be7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd3b3887902d017f0f45",
+    "url": "/static/css/main.75b1efcb.chunk.css"
+  },
+  {
+    "revision": "7825f51c0b36090b7867",
+    "url": "/static/js/3.e057bcea.chunk.js"
+  },
+  {
+    "revision": "8d26b2e3700774aaa3e681d097f59854",
+    "url": "/static/js/3.e057bcea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf30265f13b58682ca74",
+    "url": "/static/js/4.7928ddf2.chunk.js"
+  },
+  {
+    "revision": "c868efc3877e1c165bf3",
+    "url": "/static/js/5.445d8aff.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/5.445d8aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c02c23795cc5382d3c",
+    "url": "/static/js/6.79fc86d9.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/static/js/6.79fc86d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3b3887902d017f0f45",
+    "url": "/static/js/main.5cae8653.chunk.js"
+  },
+  {
+    "revision": "e752fb39828875cea660",
+    "url": "/static/js/runtime-main.33d03ef4.js"
+  },
+  {
+    "revision": "598a9df1edf030d1c0de",
+    "url": "/static/js/xlsx.b60bc61d.chunk.js"
+  },
+  {
+    "revision": "be1cc8c6decc1a56c8e27eeccd613762",
+    "url": "/static/media/about2.be1cc8c6.png"
+  },
+  {
+    "revision": "2497ba7610d1d156e4409f29946b86bc",
+    "url": "/static/media/cough.2497ba76.svg"
+  },
+  {
+    "revision": "f1f96e75fa015fe715934cbfee0b4059",
+    "url": "/static/media/coughing__monochromatic.f1f96e75.svg"
+  },
+  {
+    "revision": "9355c509677079a4dfbb30cb70825b95",
+    "url": "/static/media/health.9355c509.svg"
+  },
+  {
+    "revision": "ddcede2e8e7291c25585c23b2faf7d70",
+    "url": "/static/media/loader.ddcede2e.svg"
+  },
+  {
+    "revision": "21d1a867a60e8a6f65b8e0229e4af4aa",
+    "url": "/static/media/patient.21d1a867.svg"
+  },
+  {
+    "revision": "a2f06c089883a32f42812ef892de66c5",
+    "url": "/static/media/virus.a2f06c08.png"
+  },
+  {
+    "revision": "cc06292c6a56e1b800ac327797c6aee7",
+    "url": "/static/media/virus.cc06292c.svg"
+  }
+]);
---
+++ docs/static/js/xlsx.b60bc61d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcovid19=this.webpackJsonpcovid19||[]).push([[2],{330:function(n,c){},419:function(n,c){},420:function(n,c){}}]);
+//# sourceMappingURL=xlsx.b60bc61d.chunk.js.map
